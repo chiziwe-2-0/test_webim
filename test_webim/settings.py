3a2462c33b070458683c9bb62c36da82b5e2aeab
Ne kit <chiziwe@ya.ru>
2020-07-16 23:50:14 +0300

    v1.2

--- test_webim/settings.py
+++ test_webim/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.midleware.WhiteNoiseMiddleware'
+   # 'whitenoise.midleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
